8726bac155a716a0c6c3a1ef72cd0b81c5ce6e70
saitejapobbathi <129029843+saitejapobbathi@users.noreply.github.com>
2023-03-27 10:34:07 +0530

    Add files via upload

---
+++ day2Program04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class program04
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();//15
+int  i;//variable declaration
+i=num;  //initialization
+while(i>=0)
+{
+	System.out.print(i+",");
+	i=i-5;
+}//while loop
+
+}//main()
+}//program11
+//+,-,*,1,%
