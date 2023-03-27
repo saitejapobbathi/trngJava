49972821cc9185241a7bc5a06afc7b39be8aa948
saitejapobbathi <129029843+saitejapobbathi@users.noreply.github.com>
2023-03-27 10:35:03 +0530

    Add files via upload

---
+++ day2Program05.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class program05
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num=scob.nextInt();//24
+if(num%2==0){System.out.print("EVEN");}
+if(num%2 !=0){System.out.print("ODD");}
+}//main()
+}//program05
